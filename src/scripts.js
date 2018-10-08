d0bdaec690457cd6e2781afbbf09ca172ebad9a8
Duncan <marjoribanks.d@gmail.com>
2018-10-08 13:43:05 +0100

    fixed some typos in comments

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
 console.log("content loaded");
 
-//chexk off todos by clicking
 $('ul').on("click", "li", function(){
   $(this).toggleClass("completed");
 });
 
-//click on x to delete todo
 $('ul').on("click", "span", function(event){
   event.stopPropagation();
   $(this).parent().fadeOut(500, function(){
@@ -14,16 +12,12 @@ $('ul').on("click", "span", function(event){
   });
 });
 
-//add a new todo
 $("input[type='text']").keypress(function(event){
-  //checking for an enter press
   if (event.which === 13){
     let todoText = $(this).val();
     $(this).val("");
-    //create a new li and add to ul
     $("ul").append("<li><span><i class='fa fa-trash'></i> </span>" + todoText + "</li>");
-  }
+  };
 });
-
 
 });
