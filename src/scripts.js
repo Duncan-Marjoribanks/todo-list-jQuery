7a1c92b5e48d7db97027df0bf0bb3a903d59ec16
Duncan <marjoribanks.d@gmail.com>
2018-10-08 11:35:03 +0100

    add item functionality added

--- src/scripts.js
+++ src/scripts.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
 console.log("content loaded");
 
 //chexk off todos by clicking
-$('li').click(function(){
+$('ul').on("click", "li", function(){
   $(this).toggleClass("completed");
 });
 
 //click on x to delete todo
-$('span').click(function(event){
+$('ul').on("click", "span", function(event){
   event.stopPropagation();
   $(this).parent().fadeOut(500, function(){
       $(this).remove();
@@ -15,7 +15,15 @@ $('span').click(function(event){
 });
 
 //add a new todo
-$('input').on("")
+$("input[type='text']").keypress(function(event){
+  //checking for an enter press
+  if (event.which === 13){
+    let todoText = $(this).val();
+    $(this).val("");
+    //create a new li and add to ul
+    $("ul").append("<li><span>X </span>" + todoText + "</li>");
+  }
+});
 
 
 });
